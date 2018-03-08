d0b58eba162b9515b9067eb932205e6009e03bb5
paskinelcamacho <paskycamacho@gmail.com>
2018-03-08 08:58:36 -0600

    carga comentarios.php

---
+++ cometarios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="utf-8">
+ <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<h1> Comentarios</h1>
+
+<div>
+<FORM ACTION="enviar_email.php" METHOD="post">
+<label class="etiquetas" for="nombre">Nombre</label> <input class="cajas" type="text" size=36 name="nombre"></input><br/>
+<label class="etiquetas" for="cometarios">Comentarios</label> <input class="cajas" type="text" size=36 name="paterno"></input><br/>
+
+<label class="etiquetas" for="email">Correo electrónico</label> <input class="cajas" type="text" size=36 name="email"></input><br/>
+
+<INPUT type="submit" value="Enviar">
+</div>
+</FORM>
+</body>
+</html>
+
+
+
