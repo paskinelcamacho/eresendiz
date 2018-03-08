11576b880af4550109472c06d38fa6e144e14a71
paskinelcamacho <paskycamacho@gmail.com>
2018-03-08 08:54:33 -0600

    segunda carga

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<title>Examen Web 2.0</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<h3>EDGAR PASKINEL RESENDIZ CAMACHO.</h3>
+		<h4>Licenciatura INFORMÁTICA</h4>
+	</header>
+	<main>
+		<div id="favoritas">
+			<ul>
+				<li> Favoritas</li>
+				<li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/2005/informatica/3/1365.pdf" class="asignaturas">Desarrollo de aplicaciones en manejadores de bases de datos relacionales </a></li>
+				<li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/2012/informatica/1/1169.pdf" class="asignaturas">Informática I (fundamentos) </a></li>
+				<li> <a href="http://licenciaturas.fca.unam.mx/docs/informatica/plan_2012/02_semestre/01_informatica_2.pdf" class="asignaturas">Informática II (Administración de requerimientos </a></li>
+				<li> <a href="http://fcaenlinea1.unam.mx/clases_virtuales/informatica/1365.html" class="asignaturas">Bases de datos </a></li>
+				<li> <a href="http://www.dgire.unam.mx/contenido/normatividad/lic/planes_estudio/infor_2011/OPTPROFE/AREA_REDES_TELE/0375.pdf" class="asignaturas">Desarrollo de aplicaciones para la Web 2.0 </a></li>
+
+			</ul>
+		</div>
+
+		<div id="no_favoritas">
+                        <ul>
+                                <li>No favoritas</li>
+                                <li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/20172/contaduria/1/apunte/LC_1158_14116_A_contabilidad1.pdf" class="asignaturas">Contabilidad </a></li>
+                                <li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/2012/administracion/1/1141.pdf" class="asignaturas">Entorno de las organizaciones </a></li>
+                                <li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/98/2/mate_fin.pdf" class="asignaturas">Matematicas financieras </a></li>
+                                <li> <a href="http://fcasua.contad.unam.mx/apuntes/interiores/docs/2012/administracion/6/1625.pdf" class="asignaturas">Finanzas corporativas </a></li>
+                                <li> <a href="http://fcaenlinea1.unam.mx/clases_virtuales/informatica/1667.html" class="asignaturas">Matemáticas VI (Investigación de operaciones.  </a></li>
+
+                        </ul>
+                </div>
+
+
+
+
